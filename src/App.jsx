288861db4797fab6905e94d01db3957c9d72ccca
Santosh <sy59021321@student.ku.edu.np>
2023-03-13 18:22:58 +0545

    Setting Up Project

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div>
+      <h1>LinkedUs</h1>
+      <p>On progress....</p>
+      <button>Collaborate</button>
+    </div>
+  )
+}
