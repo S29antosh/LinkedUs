3d2924cecc72f05db55a803c6b20682e49baea76
Santosh <sy59021321@student.ku.edu.np>
2023-03-28 23:58:19 +0545

    Logo defined

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 
+
+
 export default function App() {
   return (
     <div>
-      <h1>LinkedUs</h1>
+      <h1>linkedUs</h1>
       <p>On progress....</p>
       <button>Collaborate</button>
     </div>
